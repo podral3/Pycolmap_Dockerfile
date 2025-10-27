3ac47ca4c10d8898a4b5a50c9174319c7c7a8751
podral3 <podral3@gmail.com>
2025-10-27 21:19:55 +0100

    added camera params loading from json

---
+++ camera.py
@@ -0,0 +1,21 @@
+import pycolmap, json
+
+def camera_from_json(path) -> pycolmap.Camera:
+    with open(path, "r", encoding="utf-8") as f:
+        camera_data = json.load(f)
+
+    return pycolmap.Camera(
+        model=camera_data["model"],
+        width=camera_data["width"],
+        height=camera_data["height"],
+        params=camera_data["params"]
+    )
+
+def get_imageReaderOptions(camera: pycolmap.Camera, mask_path):
+    imageReaderOptions = pycolmap.ImageReaderOptions
+    imageReaderOptions.camera_params = camera.params_to_string()
+    imageReaderOptions.camera_mask_path = mask_path
+    return imageReaderOptions
+
+camera = camera_from_json('camera.json')
+get_imageReaderOptions(camera)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pycolmap
 from pathlib import Path
+from camera import camera_from_json, get_imageReaderOptions
 
 image_dir = Path("images")
 output_path = Path("output")
@@ -15,11 +16,15 @@ database = db.open(str(db_path))
 
 print(f"Database created at: {db_path}")
 
+
+camera = camera_from_json("camera.json")
+reader_options = get_imageReaderOptions(camera, "masks")
 # Extract features from images
 if image_dir.exists():
     pycolmap.extract_features(database_path=str(db_path),
                                image_path=str(image_dir),
-                                 )
+                               camera_mode=pycolmap.CameraMode.SINGLE,
+                               reader_options= reader_options)
     pycolmap.match_exhaustive(str(db_path))
     maps = pycolmap.incremental_mapping(str(db_path), 
                                         image_path= str(image_dir),
